3bfc4526c854b68f062bd361c9391f3ac1ca05d9
NgoakoM <4202406@myuwc.ac.za>
2026-05-14 22:19:22 +0200

    post page

---
+++ MedLink/src/components/AddModel/addModal.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import maleUser from "../../assets/male user proile.png";
+
+const AddModal = () => {
+    return (
+        <div className=''>
+            <div className='flex gap-4 items-center'>
+                <div className='relative'>
+                    <img src={maleUser} className='w-15 h-15 rounded-full' alt="img" />
+                </div>
+                <div className='text-2xl'>Dr. N Malowa</div>
+            </div>
+
+            <div> 
+                <textarea cols={50} rows={5} placeholder='What do you want to talk about? ' className='my-3 outline-0 text-xl p-2'></textarea>
+            </div>
+            <div>
+                <img className='w-20 h-20 rounded-xl' src="https://i.fbcd.co/products/original/a-medical-illustration-background-for-worlds-health-day-with-copy-space-4-cf3c72b8b3657425f48024f3055903e4f8ac17cd3cea2666b4277f9448c846ae.jpg"/>
+            </div>
+        </div>
+    )
+}
+
+export default AddModal
--- MedLink/src/pages/Feeds/feeds.jsx
+++ MedLink/src/pages/Feeds/feeds.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../../components/Card/card'
 import ProfileCard from '../../components/ProfileCard/profileCard'
 import maleUser from "../../assets/male user proile.png";
@@ -7,8 +7,15 @@ import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
 import ArticleIcon from '@mui/icons-material/Article';
 import Advertisement from '../../components/Advertisement/advertisement';
 import Post from '../../components/Post/post';
-
+import Modal from '../../components/Modal/modal';
+import AddModal from '../../components/AddModel/addModal';
 const Feeds = () => {
+
+  const [addPostModal, setAddPostModal] = useState(false);
+
+  const handleOpenPostModal = () => {
+    setAddPostModal(prev=>!prev)
+  }
   return (
     <div className='px-5 xl:px-50 py-9 flex gap-5 w-full mt-5 bg-gray-100'>
 
@@ -41,13 +48,13 @@ const Feeds = () => {
           <Card padding={1}>
             <div className='flex gap-2 items-center'>
               <img src={maleUser} className='rounded-4xl w-13 h-13 border-2 border-white cursor-pointer' />
-              <div className='w-full border-1 py-3 px-3 rounded-3xl cursor-pointer hover:bg-gray-100'> Start a post</div>
+              <div onClick={() => setAddPostModal(true)} className='w-full border-1 py-3 px-3 rounded-3xl cursor-pointer hover:bg-gray-100'> Start a post</div>
             </div>
 
             <div className='w-full flex mt-3'>
-              <div className='flex gap-2 cursor-pointer justify-center rounded-lg w-[33%] hover:bg-gray-100'><VideoCallIcon sx={{ color: "green" }} />Video</div>
-              <div className='flex gap-2 cursor-pointer justify-center rounded-lg w-[33%] hover:bg-gray-100'><InsertPhotoIcon sx={{ color: "red" }} />Photo</div>
-              <div className='flex gap-2 cursor-pointer justify-center rounded-lg w-[33%] hover:bg-gray-100'><ArticleIcon sx={{ color: "yellow" }} />Article</div>
+              <div onClick={() => setAddPostModal(true)} className='flex gap-2 cursor-pointer justify-center rounded-lg w-[33%] hover:bg-gray-100'><VideoCallIcon sx={{ color: "green" }} />Video</div>
+              <div onClick={() => setAddPostModal(true)} className='flex gap-2 cursor-pointer justify-center rounded-lg w-[33%] hover:bg-gray-100'><InsertPhotoIcon sx={{ color: "red" }} />Photo</div>
+              <div onClick={() => setAddPostModal(true)} className='flex gap-2 cursor-pointer justify-center rounded-lg w-[33%] hover:bg-gray-100'><ArticleIcon sx={{ color: "yellow" }} />Article</div>
 
             </div>
           </Card>
@@ -88,7 +95,12 @@ const Feeds = () => {
           <Advertisement />
         </div>
       </div>
+      {
+        addPostModal && <Modal closeModal={handleOpenPostModal} title={""}>
+          <AddModal />
 
+        </Modal>
+      }
     </div>
   )
 }
